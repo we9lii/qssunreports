d39f74ce4599e0b1d68c2b6de6f8956a7bab4895
Faisal <we9li@hotmail.com>
2025-09-04 09:24:54 +0300

    أضفت إعدادات static وcollectstatic

--- myproject/settings.py
+++ myproject/settings.py
@@ -131,3 +131,11 @@ import os
 # ملفات الوسائط (المرفقات)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# إعدادات الملفات الثابتة للنشر
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
